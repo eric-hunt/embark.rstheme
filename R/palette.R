4ff8d05066615303ca4f4f04635c7d51d8235336
Eric Hunt <52219938+eric-hunt@users.noreply.github.com>
2023-06-30 13:51:51 -0400

    Added official palette colors

---
+++ R/palette.R
@@ -0,0 +1,31 @@
+#' Embark official color palette
+#'
+#' These are the official colors as listed on
+#' the [Embark Webpage](https://embark-theme.github.io/)
+#' in June 2023.
+#' @source https://embark-theme.github.io/
+#'
+embark_official <- list(
+  # Space
+  space1 = "#100E23",
+  space2 = "#1E1C31",
+  space3 = "#2D2B40",
+  space4 = "#3E3859",
+  space5 = "#585273",
+  # Astral
+  astral1 = "#8A889D",
+  astral2 = "#CBE3E7",
+  # Nebula
+  nebula01 = "#F48FB1",
+  nebula02 = "#A1EFD3",
+  nebula03 = "#FFE6B3",
+  nebula04 = "#91DDFF",
+  nebula05 = "#D4BFFF",
+  nebula06 = "#ABF8F7",
+  nebula07 = "#F02E6E",
+  nebula08 = "#7FE9C3",
+  nebula09 = "#F2B482",
+  nebula10 = "#78A8FF",
+  nebula11 = "#7676FF",
+  nebula12 = "#63F2F1"
+)
